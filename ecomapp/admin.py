1def9b793424978e391c59e548e5fd7ca10d6273
ManishaBelsare <manishabelsare57@gmail.com>
2024-07-10 17:09:42 +0530

    initial commit

---
+++ ecomapp/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from ecomapp.models import Product, Contactenquiry
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'age', 'cat', 'pdetail', 'is_active']
+    list_filter = ['cat', 'is_active']
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'message']
+
+
+admin.site.register(Contactenquiry, ContactAdmin)
